39419317174d88f49f117259f1422a9998ddcb58
KtGunn <ktgunn@hotmail.com>
2026-07-21 14:18:31 -0400

    AnotherRobot function created.

--- application.go
+++ application.go
@@ -28,6 +28,42 @@ var Robots []*Robot
 
 
 
+
+/*
+   func CreateAppInstance(windowDims fyne.Size, banks []*Bank) {
+
+  ApplicationInstance = NewApp(windowDims)
+	content := container.NewHBox()
+
+	for _, bank := range banks {
+		for _, car := range bank.Cars {
+			NewElevator(bank.Name, car.Name)
+		}
+	}
+}
+*/
+
+func NewElevator(bank string, car string, landings []*Landing,
+	dims fyne.Size) (CabinObject, *fyne.Container){
+
+	cabinObj := NewCabinObject(bank, car)
+	levels := CabinToLevels(landings)
+	
+	cabinObj.elevator = CreateElevatorCabin(dims, levels)
+	
+	cab := container.NewWithoutLayout(cabinObj.elevator.background)
+	cab.Add(cabinObj.elevator.car.container)
+	cabinObj.elevator.Place(0)
+
+	return cabinObj, cab
+}
+
+
+func AnotherRobot(name string,dims CarDimensions) *Robot {
+		return CreateRobot(name, dims)
+}
+
+
 func CreateAppInstance(windowDims fyne.Size, banks []*Bank) {
 
   ApplicationInstance = NewApp(windowDims)
@@ -37,17 +73,13 @@ func CreateAppInstance(windowDims fyne.Size, banks []*Bank) {
 	for _, bank := range banks {
 		for _, car := range bank.Cars {
 
-			cabinObj := NewCabinObject(bank.Name, car.Name)
-			levels := CabinToLevels(car.Landings)
-
-			cabinObj.elevator = CreateElevatorCabin(windowDims, levels)
-
-			cab := container.NewWithoutLayout(cabinObj.elevator.background)
-			cab.Add(cabinObj.elevator.car.container)
-			cabinObj.elevator.Place(0)
-
+			cabinObj, cab := NewElevator(bank.Name, car.Name, car.Landings, windowDims)
+			
 			if len(CabinObjects) == 0 {
-				robot := CreateRobot("Tug", cabinObj.elevator.dimensions.car)
+				robot := AnotherRobot("Tug", cabinObj.elevator.dimensions.car)
+				/*
+				  robot := CreateRobot("Tug", cabinObj.elevator.dimensions.car)
+				*/
 				Robots = append(Robots, robot)
 				robot.AssignCar(cabinObj.elevator.car)
 				robot.SetFloorState(PCOL_LOBBY)
